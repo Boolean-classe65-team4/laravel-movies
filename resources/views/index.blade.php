08ea4c17492a4623f70f79f57b6531cea152f0a5
LucaSk-x <lucapiccinno64@gmail.com>
2022-09-02 16:50:44 +0200

    Add Home + home Controller function

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,11 @@
-{{-- @extends('layout.app')
-@section('page_title', 'Dettagli Film #' . $movie->id)
-@section('page_content')
+@extends('layouts.app')
+@section('content')
     <div class="container">
-
+      <div class="row justify-content-center">
+        <div class="col text-center">
+          <h1 class="p-4">Laravel Movies Team #4 </h1>
+          <a class="btn btn-outline-primary" href="{{route('movies.index')}}"><h3>Movies</h3></a>
+        </div>
+      </div>
     </div>
-@endsection --}}
+@endsection
